0cb57753219b4dd6c51345e4386d26fe2a748782
Osama Muhammad <59265090+osamamuhammad3623@users.noreply.github.com>
2020-10-21 02:06:36 +0200

    Update left_rotations-HackerRank.cpp

--- left_rotations-HackerRank.cpp
+++ left_rotations-HackerRank.cpp
@@ -13,12 +13,15 @@ int main() {
     cin >> n >> shifts;
     vector <int> nums(n);
     for (int i = 0; i < n; i++) { cin >> nums[i]; }
-
+    
+    // we make a temp vector includes the elements that should be shifted
     vector <int> shifted(shifts);
     for (int i = 0; i < shifts; i++) { shifted[i] = nums[i]; }
-
+    
+    // then we remove those elements from the front
     nums.erase(nums.begin(), nums.begin() + shifts);
 
+    // and add them in the back!
     for (int i = 0; i < shifts; i++) {
         nums.push_back(shifted[i]);
     }
