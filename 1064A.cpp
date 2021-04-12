7d609b3c4f7ef486a8639f1c513497e668a5d39c
Osama Muhammad <59265090+osamamuhammad3623@users.noreply.github.com>
2021-04-12 19:33:52 +0200

    Create 1064A.cpp

---
+++ 1064A.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <array>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int arr[3];
+    cin >> arr[0] >> arr[1] >> arr[2];
+    sort(arr, arr+3);
+    // sorted in increasing order
+
+    if (arr[0] + arr[1] > arr[2]){
+        cout << 0;
+    }
+    else{
+        cout << arr[2] - arr[1] - arr[0] +1;
+    }
+}
