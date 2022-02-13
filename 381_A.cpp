f817a715b1ef4896073518f0708025dc5cbc093a
Osama Muhammad <59265090+osamamuhammad3623@users.noreply.github.com>
2022-02-13 13:01:18 +0200

    Update 381_A.cpp

--- 381_A.cpp
+++ 381_A.cpp
@@ -1,46 +1,39 @@
 #include <iostream>
-#include <vector>
-
+#include <deque>
 using namespace std;
 
-int main() {
+int main(void) {
 	int n;
 	cin >> n;
-	
-	vector<int> numbers(n);
-	for (short i = 0; i < n; i++) { cin >> numbers[i]; }
-
-	int S{}, D{};
-
-	for (int i = 1; i <= n; i++) {
-
-		int last_element = numbers.size() - 1;
-		// in S turns:
-		if (i % 2 == 1) {
-			// if most left > most right --> update S score with the most left value, then delete it
-			if (numbers[0] > numbers[last_element]) {
-				S += numbers[0];
-				numbers.erase(numbers.begin());
-			}
-			else {
-				// if most right > most left
-				S += numbers[last_element];
-				numbers.erase(numbers.end()-1);
-			}
-		}
-		// in D turns:
-		else {
-			if (numbers[0] > numbers[last_element]) {
-				D += numbers[0];
-				numbers.erase(numbers.begin());
-			}
-			else {
-				D += numbers[last_element];
-				numbers.erase(numbers.end()-1);
-			}
-
-		}		
+	deque<int> num(n);
+	for (int i = 0; i < n; i++) {
+		cin >> num[i];
 	}
 
-	cout << S << " " << D << endl;
+	int S { }, D { };
+	int cnt = 0;
+	while (num.size() > 0) {
+		if (cnt % 2 == 0) {
+			// S turn
+			if (num[0] > num[num.size() - 1]) {
+				S += num[0];
+				num.pop_front();
+			} else {
+				S += num[num.size() - 1];
+				num.pop_back();
+			}
+		} else {
+			// D turn
+			if (num[0] > num[num.size() - 1]) {
+				D += num[0];
+				num.pop_front();
+			} else {
+				D += num[num.size() - 1];
+				num.pop_back();
+			}
+		}
+		cnt++;
+	}
+
+	cout << S << " " << D;
 }
