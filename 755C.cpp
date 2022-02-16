4b45e11b42fd4c74298e82be1293104111e8be38
Osama Muhammad <59265090+osamamuhammad3623@users.noreply.github.com>
2022-02-16 15:00:44 +0200

    Update 755C.cpp

--- 755C.cpp
+++ 755C.cpp
@@ -1,28 +1,15 @@
 #include <iostream>
 #include <vector>
-#include <stack>
 using namespace std;
 
 void dfs(vector<vector<int>> *g, int start, vector<bool> *vi){
-
-    stack<int> st;
 
     (*vi)[start] = 1; // mark starting node as visited
 
     // add neighbours to the stack
     for (auto neighbour: (*g)[start]){
-        st.push(neighbour);
-    }
-
-    while(!st.empty()){
-        int current = st.top();
-        if ((*vi)[current]){
-            st.pop();
-        }else{
-            (*vi)[current] = 1;
-            for (auto neighbour: (*g)[current]){
-                st.push(neighbour);
-            }
+        if (!(*vi)[neighbour]){
+        	dfs(g,neighbour, vi);
         }
     }
 }
@@ -30,7 +17,7 @@ void dfs(vector<vector<int>> *g, int start, vector<bool> *vi){
 int main(void) {
 	int n; cin >> n;
 
-	vector <int> v(n+1);
+	vector <int> v(n+1,0);
 
 	for (int i=1; i<= n;i++){  cin >> v[i];}
 
@@ -38,10 +25,8 @@ int main(void) {
 
 	vector<vector<int>> g(n+1);
 	for (int i=1; i<= n;i++){
-		int to = v[i];
-
-		g[i].push_back(to);
-		g[to].push_back(i);
+		g[i].push_back(v[i]);
+		g[v[i]].push_back(i);
 	}
 
 
